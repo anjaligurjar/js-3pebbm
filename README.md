79fe53b82304219e46c714aaabfe23e52d3ddd23
anjaligurjar <anjaligurjar861@gmail.com>
2023-06-07 18:08:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-3pebbm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-3pebbm)
---
+++ script.js
@@ -0,0 +1,11 @@
+
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const circle = document.querySelector('.circle');
+  const pages = document.querySelectorAll('.page');
+  const loginForm = document.getElementById('loginForm');
+  const registrationForm = document.getElementById('registrationForm');
+  const loginBtn = document.getElementById('loginBtn');
+})
+
